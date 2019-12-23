6a323f2ec3f5f3091a88083aa13f0f88f15341db
Bewvy Worapat <nokhook_bew@hotmail.com>
2019-12-23 15:13:43 +0700

    add login path

--- src/main/java/bewvy/se/cooperativev2/controllers/UserController.java
+++ src/main/java/bewvy/se/cooperativev2/controllers/UserController.java
@@ -4,9 +4,12 @@ import bewvy.se.cooperativev2.models.User;
 import bewvy.se.cooperativev2.services.UserService;
 import bewvy.se.cooperativev2.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,6 +18,10 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    private String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
 
     @Autowired
     UserService userService;
@@ -30,9 +37,18 @@ public class UserController {
         return ResponseEntity.ok().body(res);
     }
 
+    @GetMapping("/email")
+    public ResponseEntity<Optional<User>> showDataByEmail(@RequestBody User user) throws ResourceNotFoundException {
+        Optional<User> res = userService.getUserByEmail(user.getEmail());
+        return ResponseEntity.ok().body(res);
+    }
+
+
     @PostMapping
     public User addUser(@RequestBody User user) {
+        user.setPassword(hashPassword(user.getPassword()));
         userService.addUser(user);
+        System.out.println(user.getPassword());
         return user;
     }
 
@@ -49,5 +65,24 @@ public class UserController {
     public ResponseEntity<Optional<User>> editUser(@PathVariable int id, @RequestBody User userDetails) throws ResourceNotFoundException {
         final Optional<User> userUpdated = userService.updateUser(id, userDetails);
         return ResponseEntity.ok(userUpdated);
+    }
+
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> login(@Valid @RequestBody User user) {
+
+        Optional<User> dbUser = userService.getUserByEmail(user.getEmail());
+
+        if (!dbUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+
+        String hash  = dbUser.get().getPassword();
+
+        if (!BCrypt.checkpw(user.getPassword(), hash)) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Wrong Password!");
+        }
+
+        return ResponseEntity.ok("Welcome " + dbUser.get().getEmail());
     }
 }
