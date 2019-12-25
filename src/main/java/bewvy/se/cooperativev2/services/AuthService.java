52bb94b1c253c9ddb6ebad3a445da81fe1a8b0ca
Bewvy Worapat <nokhook_bew@hotmail.com>
2019-12-25 11:39:24 +0700

    add generate token

---
+++ src/main/java/bewvy/se/cooperativev2/services/AuthService.java
@@ -0,0 +1,26 @@
+package bewvy.se.cooperativev2.services;
+
+import bewvy.se.cooperativev2.models.Token;
+import bewvy.se.cooperativev2.models.User;
+import bewvy.se.cooperativev2.repositories.TokenRepository;
+import bewvy.se.cooperativev2.utils.TokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class AuthService {
+    @Autowired
+    TokenRepository tokenRepository;
+
+    public String generateToken(User user) {
+        String token = TokenUtil.getInstance().getRandomToken();
+        Token t = new Token();
+        t.setToken(token);
+        t.setUsers(Collections.singleton(user));
+        tokenRepository.save(t);
+        return token;
+    }
+
+}
